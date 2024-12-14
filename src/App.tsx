f7e282f7d8aa4aaf7bc5bf7f8da7ec6f13e3f23d
Arnel Pezo <arnel.pezo@eurobit.ba>
2024-12-14 19:49:19 +0100

    edit look a bit

--- src/App.tsx
+++ src/App.tsx
@@ -95,11 +95,12 @@ function App() {
       {(question.PlayOnlyAudio || question.PlayOnlyAudio === undefined)
         && <div className="absolute inset-0 bg-black opacity-50 z-0 w-screen h-screen" />}
 
-      <div className={`relative flex ${(question.PlayOnlyAudio || question.PlayOnlyAudio === undefined)
+      <div className={`relative flex flex-col ${(question.PlayOnlyAudio || question.PlayOnlyAudio === undefined)
                                         ? "items-center" : "items-end"} justify-center w-full h-full z-10`}>
         <h1 className="text-[40px] leading-[1.4] px-9 text-white text-center">
-          {question.NumberOfQuestionInCategory}. {qora ? question.Answer : question.Question}
+          {question.NumberOfQuestionInCategory}. {question.Question}:
         </h1>
+        <h1 className="text-[40px] font-bold leading-[1.4] px-9 text-white text-center">{qora && question.Answer}</h1>
       </div>
 
       <div className="flex justify-center gap-5 items-end m-10 z-10">
--- src/Data.ts
+++ src/Data.ts
@@ -8,7 +8,8 @@ export interface QANDA {
 };
 
 export enum Category {
-    MOVIES,
+    MOVIESANDSERIES,
+    SIENCEANDTECHNOLOGY,
     GAMES,
     HISTORY,
     GEOGRAPHY,
@@ -16,98 +17,196 @@ export enum Category {
 };
 
 export const Data: QANDA[] = [
+    // MOVIESANDSERIES
     {
-        Question: "Who directed the movie 'Inception'?",
-        Answer: "Christopher Nolan",
-        Category: Category.MOVIES,
+        Question: "Film 'Objegla Mlada' je snimljen 1999. a glavnu ulogu igra?",
+        Answer: "Julia Roberts",
+        Category: Category.MOVIESANDSERIES,
         NumberOfQuestionInCategory: 1,
-        YTVideoLink: "https://www.youtube.com/watch?v=q7DfQMPmJRI",
-        PlayOnlyAudio: true,
     },
     {
-        Question: "What is the highest-selling video game of all time?",
-        Answer: "Minecraft",
-        Category: Category.GAMES,
-        NumberOfQuestionInCategory: 1
-    },
-    {
-        Question: "In which year did World War II end?",
-        Answer: "1945",
-        Category: Category.HISTORY,
-        NumberOfQuestionInCategory: 1
-    },
-    {
-        Question: "What is the capital city of Australia?",
-        Answer: "Canberra",
-        Category: Category.GEOGRAPHY,
-        NumberOfQuestionInCategory: 1
-    },
-    {
-        Question: "Which country won the FIFA World Cup in 2018?",
-        Answer: "France",
-        Category: Category.SPORT,
-        NumberOfQuestionInCategory: 1
-    },
-    {
-        Question: "Which actor played the Joker in 'The Dark Knight'?",
-        Answer: "Heath Ledger",
-        Category: Category.MOVIES,
+        Question: "Koji je glumac poznat po ulozi Iron Mana u Marvelovim filmovima?",
+        Answer: "Robert Downey Jr.",
+        Category: Category.MOVIESANDSERIES,
         NumberOfQuestionInCategory: 2,
-        YTVideoLink: "https://www.youtube.com/watch?v=FssULNGSZIA",
-        PlayOnlyAudio: false,
     },
     {
-        Question: "What is the name of the protagonist in 'The Legend of Zelda' series?",
-        Answer: "Link",
+        Question: "Koja serija prati život porodice Stark u fantastičnom svijetu Westerosa?",
+        Answer: "Igra prijestolja (Game of Thrones)",
+        Category: Category.MOVIESANDSERIES,
+        NumberOfQuestionInCategory: 3,
+    },
+    {
+        Question: "Koji je redatelj poznat po filmovima 'Inception' i 'Interstellar'?",
+        Answer: "Christopher Nolan",
+        Category: Category.MOVIESANDSERIES,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koji je najskuplji film ikada snimljen?",
+        Answer: "Avatar: The Way of Water",
+        Category: Category.MOVIESANDSERIES,
+        NumberOfQuestionInCategory: 5,
+    },
+
+    // SIENCEANDTECHNOLOGY
+    {
+        Question: "Koje planete su prve komšije zemlje?",
+        Answer: "Venera i Mars",
+        Category: Category.SIENCEANDTECHNOLOGY,
+        NumberOfQuestionInCategory: 1,
+    },
+    {
+        Question: "Koji uređaj koristimo za mjerenje vremena?",
+        Answer: "Sat",
+        Category: Category.SIENCEANDTECHNOLOGY,
+        NumberOfQuestionInCategory: 2,
+    },
+    {
+        Question: "Koji je prvi čovjek na Mjesecu?",
+        Answer: "Neil Armstrong",
+        Category: Category.SIENCEANDTECHNOLOGY,
+        NumberOfQuestionInCategory: 3,
+    },
+    {
+        Question: "Koja teorija se koristi za objašnjenje širenja svemira?",
+        Answer: "Teorija Velikog praska",
+        Category: Category.SIENCEANDTECHNOLOGY,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koja godine smo imali prvi računar i kako se zvao?",
+        Answer: "Eniac (1945)",
+        Category: Category.SIENCEANDTECHNOLOGY,
+        NumberOfQuestionInCategory: 5,
+    },
+
+    // GAMES
+    {
+        Question: "Koji je lik glavni heroj u igrici 'Super Mario Bros'?",
+        Answer: "Mario",
         Category: Category.GAMES,
-        NumberOfQuestionInCategory: 2
+        NumberOfQuestionInCategory: 1,
     },
     {
-        Question: "Who was the first president of the United States?",
+        Question: "Koji je studio kreirao igricu 'Minecraft'?",
+        Answer: "Mojang",
+        Category: Category.GAMES,
+        NumberOfQuestionInCategory: 2,
+    },
+    {
+        Question: "Koja je igrica poznata po popularnom liku Lara Croft?",
+        Answer: "Tomb Raider",
+        Category: Category.GAMES,
+        NumberOfQuestionInCategory: 3,
+    },
+    {
+        Question: "Koji je naziv oružja koje nosi Master Chief u igrici 'Halo'?",
+        Answer: "Energetski mač (Energy Sword)",
+        Category: Category.GAMES,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koji je najprodavaniji serijal videoigara svih vremena?",
+        Answer: "Grand Theft Auto (GTA)",
+        Category: Category.GAMES,
+        NumberOfQuestionInCategory: 5,
+    },
+
+    // HISTORY
+    {
+        Question: "Koja godina se smatra početkom Prvog svjetskog rata?",
+        Answer: "1914.",
+        Category: Category.HISTORY,
+        NumberOfQuestionInCategory: 1,
+    },
+    {
+        Question: "Ko je bio prvi predsjednik Sjedinjenih Američkih Država?",
         Answer: "George Washington",
         Category: Category.HISTORY,
-        NumberOfQuestionInCategory: 2
+        NumberOfQuestionInCategory: 2,
     },
     {
-        Question: "Which river is the longest in the world?",
-        Answer: "Nile",
-        Category: Category.GEOGRAPHY,
-        NumberOfQuestionInCategory: 2
-    },
-    {
-        Question: "In tennis, what is a score of zero called?",
-        Answer: "Love",
-        Category: Category.SPORT,
-        NumberOfQuestionInCategory: 2
-    },
-    {
-        Question: "What movie won the Oscar for Best Picture in 2020?",
-        Answer: "Parasite",
-        Category: Category.MOVIES,
-        NumberOfQuestionInCategory: 3
-    },
-    {
-        Question: "What is the main objective in the game 'Minecraft'?",
-        Answer: "To survive and build",
-        Category: Category.GAMES,
-        NumberOfQuestionInCategory: 3
-    },
-    {
-        Question: "Who was the first man to step on the moon?",
-        Answer: "Neil Armstrong",
+        Question: "Kako se zvala slavna egipatska kraljica poznata po vezi s Julijem Cezarom?",
+        Answer: "Kleopatra",
         Category: Category.HISTORY,
-        NumberOfQuestionInCategory: 3
+        NumberOfQuestionInCategory: 3,
     },
     {
-        Question: "Which country is known as the Land of the Rising Sun?",
-        Answer: "Japan",
+        Question: "Koji je poznati general prešao Alpe sa svojim vojnicima i slonovima?",
+        Answer: "Hanibal",
+        Category: Category.HISTORY,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koji je najduži rat u historiji?",
+        Answer: "Stogodišnji rat",
+        Category: Category.HISTORY,
+        NumberOfQuestionInCategory: 5,
+    },
+
+    // GEOGRAPHY
+    {
+        Question: "Koji grad leži na dva kontinenta i koja su dva kontinenta u pitanju?",
+        Answer: "Istanbul, Azija i Europa",
         Category: Category.GEOGRAPHY,
-        NumberOfQuestionInCategory: 3
+        NumberOfQuestionInCategory: 1,
     },
     {
-        Question: "Which basketball player is known as 'King James'?",
-        Answer: "LeBron James",
+        Question: "Koja rijeka teče kroz Egipat?",
+        Answer: "Nil",
+        Category: Category.GEOGRAPHY,
+        NumberOfQuestionInCategory: 2,
+    },
+    {
+        Question: "Naziv Kanada potiče iz indijanskog jezika a znaci?",
+        Answer: "Veliko selo",
+        Category: Category.GEOGRAPHY,
+        NumberOfQuestionInCategory: 3,
+    },
+    {
+        Question: "Koji je najviši planinski vrh na svijetu?",
+        Answer: "Mont Everest",
+        Category: Category.GEOGRAPHY,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koji je najmanji kontinent na svijetu?",
+        Answer: "Australija",
+        Category: Category.GEOGRAPHY,
+        NumberOfQuestionInCategory: 5,
+    },
+
+    // SPORT
+    {
+        Question: "Koji dva kluba dolaze iz istog grada a sa dva raličita kontinenta?",
+        Answer: "Galatasaray i Fenerbahce",
         Category: Category.SPORT,
-        NumberOfQuestionInCategory: 3
-    }
+        NumberOfQuestionInCategory: 1,
+    },
+    {
+        Question: "Ko je osvajao najviše Grand Slam titula u tenisu?",
+        Answer: "Novak Đoković",
+        Category: Category.SPORT,
+        NumberOfQuestionInCategory: 2,
+    },
+    {
+        Question: "Koji tim je osvojio FIFA Svjetsko prvenstvo 2006. godine?",
+        Answer: "Italija",
+        Category: Category.SPORT,
+        NumberOfQuestionInCategory: 3,
+    },
+    {
+        Question: "Koja zemlja je domaćin Olimpijskih igara 2024. godine?",
+        Answer: "Francuska (Pariz)",
+        Category: Category.SPORT,
+        NumberOfQuestionInCategory: 4,
+    },
+    {
+        Question: "Koja je najteža atletska disciplina na Olimpijadi?",
+        Answer: "Desetoboj",
+        Category: Category.SPORT,
+        NumberOfQuestionInCategory: 5,
+    },
 ];
+
