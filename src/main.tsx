2dbfcdada8e5ebd265aca12345e20820fafe0295
Arnel Pezo <arnel.pezo@eurobit.ba>
2025-06-27 19:37:02 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />);
+// createRoot(document.getElementById('root')!).render(<App />);
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// );
+// const root = ReactDOM.createRoot(document.getElementById("root")!);
+// root.render(<App />);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
